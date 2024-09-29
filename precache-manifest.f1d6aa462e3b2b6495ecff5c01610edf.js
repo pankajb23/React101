fb0009d79719813497dfac074800d8da0b87d614
pankaj bhardwaj <pankaj.bhardwaj.2100@gmail.com>
2024-09-29 18:46:23 +0530

    Updates

---
+++ precache-manifest.f1d6aa462e3b2b6495ecff5c01610edf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dce696928f132ff96d3dd143c31afe3",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "09ae083df60eb5eb306c",
+    "url": "/tic-tac-toe/static/css/main.add5c97a.chunk.css"
+  },
+  {
+    "revision": "3334fb14d0fd5a25568d",
+    "url": "/tic-tac-toe/static/js/2.be183ea5.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/tic-tac-toe/static/js/2.be183ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d327829e7d9e2be8bb48",
+    "url": "/tic-tac-toe/static/js/3.65c08383.chunk.js"
+  },
+  {
+    "revision": "09ae083df60eb5eb306c",
+    "url": "/tic-tac-toe/static/js/main.e8984ad2.chunk.js"
+  },
+  {
+    "revision": "339d540f8434de88c3a6",
+    "url": "/tic-tac-toe/static/js/runtime-main.d74f6458.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/tic-tac-toe/static/media/logo.06e73328.svg"
+  }
+]);

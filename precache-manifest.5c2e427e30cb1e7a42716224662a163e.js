99bf83c4391fcf4ef5a7a79659e8ca5641656109
pankaj bhardwaj <pankaj.bhardwaj.2100@gmail.com>
2024-09-29 18:53:58 +0530

    Updates

---
+++ precache-manifest.5c2e427e30cb1e7a42716224662a163e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6717d9eaa8fcb7aca5dc5ba5b654f4e",
+    "url": "/React101/index.html"
+  },
+  {
+    "revision": "09ae083df60eb5eb306c",
+    "url": "/React101/static/css/main.add5c97a.chunk.css"
+  },
+  {
+    "revision": "3334fb14d0fd5a25568d",
+    "url": "/React101/static/js/2.be183ea5.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/React101/static/js/2.be183ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d327829e7d9e2be8bb48",
+    "url": "/React101/static/js/3.65c08383.chunk.js"
+  },
+  {
+    "revision": "09ae083df60eb5eb306c",
+    "url": "/React101/static/js/main.e8984ad2.chunk.js"
+  },
+  {
+    "revision": "efbc683fec2c4bd29c2b",
+    "url": "/React101/static/js/runtime-main.d7db8bd8.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/React101/static/media/logo.06e73328.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React101/precache-manifest.5c2e427e30cb1e7a42716224662a163e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React101/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
